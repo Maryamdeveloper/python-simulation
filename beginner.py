3dfa7b5fc9f5a66ee8c261adfe95317976511312
Maryam Kazemi <maryamkazemit@gmail.com>
2023-07-29 22:38:53 +0330

    create a simple game to guess number by human & computer,move on string array in for loops

--- beginner.py
+++ beginner.py
@@ -1,10 +1,11 @@
+import random
 print('hello')
 type(2)
 age = input('your age: ')
 type(age)
 int(age)
 float(age)
-a,b=6,7 #declare variables in one line
+a, b = 6, 7  # declare variables in one line
 A = 6
 B = 7
 print(A)
@@ -60,7 +61,6 @@ while N >= 0:
     if N == 10:
         break
 
-
 # first hw
 numbers = []
 while True:
@@ -74,3 +74,51 @@ if len(numbers) > 0:
     print("Average:", average)
 else:
     print("No numbers were entered.")
+
+# game
+randNumber = random.randint(1, 99)
+guessedNumber = input('guess a number: ')
+while int(guessedNumber) != randNumber:
+    if (int(guessedNumber) > randNumber):
+        print('mine is smaller')
+    else:
+        print('mine is larger')
+    guessedNumber = input('guess a number: ')
+print('you guessed right')
+
+# second hw
+a = 1
+b = 99
+computerGuessed = random.randint(a, b)
+print(computerGuessed)
+userRandNumber = input(
+    'choose the state b for bigger, s for smaller, c for correct: ')
+while userRandNumber != 'c':
+    if userRandNumber == 'b':
+        b = computerGuessed
+    elif userRandNumber == 's':
+        a = computerGuessed
+    else:
+        print('the input value is not valid')
+        userRandNumber = input(
+            'choose the state b for bigger, s for smaller, c for correct: ')
+    computerGuessed = random.randint(a, b)
+    print(computerGuessed)
+    userRandNumber = input(
+        'choose the state b for bigger, s for smaller, c for correct: ')
+print('i finally guessed right')
+
+hi='salam'
+print(hi)
+print(hi[0])
+print(hi[4])
+len(hi) #length of string
+for i in range(0,len(hi)): #moving on string array method 1
+    print(i,a[i])
+count_a=0
+for letter in 'salam maryam': #moving on string array method 2
+    print(letter)
+    if letter=='a':
+        count_a=count_a+1
+
+print(count_a)
